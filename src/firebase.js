4d459ebc8016e6996740e58bb497042311858e0c
Harsh <valambeharsh1701@gmail.com>
2024-02-02 22:02:39 +0530

    feat: half done

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUMv2OWpzI9ikTZVnR1erb_YrFWrYHzbA",
+  authDomain: "disney-e287b.firebaseapp.com",
+  projectId: "disney-e287b",
+  storageBucket: "disney-e287b.appspot.com",
+  messagingSenderId: "750731086024",
+  appId: "1:750731086024:web:b5b3bd9ce23a94479a7dfd",
+  measurementId: "G-DRVLJKWRWG",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
